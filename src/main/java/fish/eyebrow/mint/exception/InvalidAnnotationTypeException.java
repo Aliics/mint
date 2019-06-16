33feb94d14bb10a4515cc8d98d1325b43c80edfc
Alexander Johnston <Aliics@hotmail.com>
2019-06-16 23:03:01 +1200

    Removed files from old structure

--- src/main/java/fish/eyebrow/mint/exception/InvalidAnnotationTypeException.java
+++
@@ -1,9 +0,0 @@
-package fish.eyebrow.mint.exception;
-
-import java.lang.reflect.Field;
-
-public class InvalidAnnotationTypeException extends Throwable {
-    public InvalidAnnotationTypeException(final Field field) {
-        super(String.format("An invalid data type was assigned on annotated field: %s", field));
-    }
-}
