8df049d007f9ebe8649bdbf7a4a53d1ea2d9e6bb
Alexander Johnston <Aliics@hotmail.com>
2019-06-24 16:44:57 +1200

    Created barebones implementation of assigned option

--- src/main/java/fish/eyebrow/mint/AssignedOptionExecutor.java
+++ src/main/java/fish/eyebrow/mint/AssignedOptionExecutor.java
@@ -1,17 +1,33 @@
 package fish.eyebrow.mint;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Field;
 
 public class AssignedOptionExecutor implements OptionExecutor<AssignedOption> {
 
     @Override
     public Class<AssignedOption> annotationType() {
-        return null;
+        return AssignedOption.class;
     }
 
 
     @Override
-    public void execute(final Object object, final String[] args) throws InvocationTargetException, IllegalAccessException {
+    public void execute(final Object object, final String[] args) throws IllegalAccessException {
+        final Field[] fields = object.getClass().getDeclaredFields();
+        for (final Field field : fields) {
+            final AssignedOption annotation = field.getAnnotation(annotationType());
+            if (field.isAnnotationPresent(annotationType())) {
+                assignAnnotated(object, args, field, annotation);
+            }
+        }
+    }
 
+
+    private void assignAnnotated(final Object object, final String[] args, final Field field, final AssignedOption annotation) throws IllegalAccessException {
+        final String option = annotation.prefix() + annotation.option();
+        for (final String arg : args) {
+            if (arg.equals(option)) {
+                field.setBoolean(object, true);
+            }
+        }
     }
 }
--- src/test/java/fish/eyebrow/mint/AssignedOptionExecutorTestCase.java
+++ src/test/java/fish/eyebrow/mint/AssignedOptionExecutorTestCase.java
@@ -1,5 +1,54 @@
 package fish.eyebrow.mint;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class AssignedOptionExecutorTestCase {
 
+    private AssignedOptionExecutor assignedOptionExecutor;
+
+    private TestClass testClass;
+
+
+    @BeforeEach
+    void setUp() {
+        assignedOptionExecutor = new AssignedOptionExecutor();
+        testClass = new TestClass();
+    }
+
+
+    @Test
+    void shouldSetBooleanFieldToTrueWhenWantedOptionIsGiven() throws IllegalAccessException {
+        final String[] args = { "--boolean-option" };
+        assignedOptionExecutor.execute(testClass, args);
+
+        assertThat(testClass.booleanOption).isTrue();
+    }
+
+
+    @Test
+    void shouldNotTouchFieldsWhenOptionsGivenDoNotContainAnnotated() throws IllegalAccessException {
+        final String[] args = { "--bad-boolean-option", "--what-is-this" };
+        assignedOptionExecutor.execute(testClass, args);
+
+        assertThat(testClass.booleanOption).isFalse();
+    }
+
+
+    @Test
+    void shouldAssignFieldWhenOptionGivenIsAmongstBadOptions() throws IllegalAccessException {
+        final String[] args = { "--bad-boolean-option", "--boolean-option", "--what-is-this" };
+        assignedOptionExecutor.execute(testClass, args);
+
+        assertThat(testClass.booleanOption).isTrue();
+    }
+
+
+    class TestClass {
+
+        @AssignedOption(option = "boolean-option")
+        boolean booleanOption = false;
+    }
 }
